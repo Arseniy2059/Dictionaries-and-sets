1c5fbe01db2523443f1878f212e982e8cff40ef9
Arseniy2059 <semyashulkovy64@mail.ru>
2024-08-16 00:26:56 +0300

    Add files via upload

---
+++ лрд.py
@@ -0,0 +1,13 @@
+name = input("my_string ")
+print(len("my_string"))
+name = ("my_string".upper())
+print(name)
+
+name = ("my_string".lower())
+print(name)
+name = ("my _ str ing".replace(" ", ""))
+print(name)
+
+a = "my_string"
+print(a[0])
+print(a[-1])

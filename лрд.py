0a2f0092a82c0ab8a92af6ae4c3c8e3d2409d39f
Arseniy2059 <semyashulkovy64@mail.ru>
2024-08-17 19:11:41 +0300

    module_1_4.py

--- лрд.py
+++ лрд.py
@@ -1,12 +1,9 @@
-name = input("my_string ")
+x = input("my_string ")
 print(len("my_string"))
-name = ("my_string".upper())
-print(name)
 
-name = ("my_string".lower())
-print(name)
-name = ("my _ str ing".replace(" ", ""))
-print(name)
+print("my_string".upper())
+print("my_string".lower())
+print("my _ str ing".replace(" ", ""))
 
 a = "my_string"
 print(a[0])
